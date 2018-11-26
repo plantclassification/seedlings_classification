e984db7a96734205f55052b2378dd2cdc1dffb38
lihaooo233 <43101480+lihaooo233@users.noreply.github.com>
2018-11-26 22:20:52 +0800

    Add files via upload
    js css fonts for 12 introduction htmls

---
+++ js2/custom.js
@@ -0,0 +1,18 @@
+// templatemo 467 easy profile
+
+// PRELOADER
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+ 
+	 		 "images2/tm-bg-slide-2.jpg"
+
+	 			], 	{duration: 3200, fade: 0});
+		});
+})
