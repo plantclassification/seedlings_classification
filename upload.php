2beb1ab791e7f435b8e066f034a7531b2b3ab374
lihaooo233 <43101480+lihaooo233@users.noreply.github.com>
2018-12-04 03:41:01 +0800

    Add files via upload
    added some functions to show photos and fixed some bugs

--- upload.php
+++ upload.php
@@ -1,5 +1,29 @@
 <?php
 
+
+//设置需要删除的文件夹
+$path = "./upload/";
+//清空文件夹函数和清空文件夹后删除空文件夹函数的处理
+function deldir($path){
+    //如果是目录则继续
+    if(is_dir($path)){
+        //扫描一个文件夹内的所有文件夹和文件并返回数组
+        $p = scandir($path);
+        if(count($p)>30){
+        foreach($p as $val){
+            //排除目录中的.和..
+            if($val !="." && $val !=".."){
+
+                    //如果是文件直接删除
+                    unlink($path.$val);
+
+            }
+        }}
+    }
+}
+//调用函数，传入路径
+deldir($path);
+
 $allowedExts = array("gif", "jpeg", "jpg", "png");
 $temp = explode(".", $_FILES["imgfile"]["name"]);
 echo $_FILES["imgfile"]["size"];
@@ -19,30 +43,28 @@ if ((($_FILES["imgfile"]["type"] == "image/gif")
     }
     else
     {
-        echo "name: " . $_FILES["imgfile"]["name"] . "<br>";
-        echo "type: " . $_FILES["imgfile"]["type"] . "<br>";
-        echo "size: " . ($_FILES["imgfile"]["size"] / 1024) . " kB<br>";
-        echo "temp location: " . $_FILES["imgfile"]["tmp_name"] . "<br>";
+       // echo "name: " . $_FILES["imgfile"]["name"] . "<br>";
+       // echo "type: " . $_FILES["imgfile"]["type"] . "<br>";
+       // echo "size: " . ($_FILES["imgfile"]["size"] / 1024) . " kB<br>";
+       // echo "temp location: " . $_FILES["imgfile"]["tmp_name"] . "<br>";
 
 
         if (file_exists("upload/" . $_FILES["imgfile"]["name"]))
-        {
-            echo $_FILES["imgfile"]["name"] . " You already uploaded the image "."<br>";
+        {    //        echo $_FILES["imgfile"]["name"] . " You already uploaded the image "."<br>";
 
-            echo"<br>"."<a href='/index.html ' style='color: crimson'>lets try again</a>";
+           // echo"<br>"."<a href='/index.html ' style='color: crimson'>lets try again</a>";
         }
-        else
-        {
+        else {
 
             move_uploaded_file($_FILES["imgfile"]["tmp_name"], "upload/" . $_FILES["imgfile"]["name"]);
-            echo "image loaction in server: " . "upload/" . $_FILES["imgfile"]["name"]. "<br>";
-            echo"<a href='/index.html ' style='color: crimson'>lets try again</a>"."<br>";
-
+            //echo "image loaction in server: " . "upload/" . $_FILES["imgfile"]["name"]. "<br>";
+            //echo"<a href='/index.html ' style='color: crimson'>lets try again</a>"."<br>";
+        }
             $output = shell_exec('C:\Users\lenovo\AppData\Local\Programs\Python\Python36\python.exe predict.py -f upload/'.$_FILES["imgfile"]["name"]);
             //$output = shell_exec('sudo python predict.py -f upload/'.$_FILES["imgfile"]["name"]);
             //$output = shell_exec('python3 predict.py '."-f upload/".$_FILES["imgfile"]["name"]);
             $t=$output;
-            rename("upload/" . $_FILES["imgfile"]["name"],"upload/" ."pictures.jpg");
+            //rename("upload/" . $_FILES["imgfile"]["name"],"upload/" ."pictures.jpg");
             //if($output=='Common wheat')
             //{
             //    echo "FQ"."<br>";
@@ -51,7 +73,7 @@ if ((($_FILES["imgfile"]["type"] == "image/gif")
             //    echo "Common wheat ";
             //    echo $output;
             //}
-
+            $kl="upload/" . $_FILES["imgfile"]["name"];
            // echo"We believe this plant belongs to ".$output;
             //sleep(5);
             //echo"<script>window.open('/'.$output);</script>";
@@ -62,82 +84,89 @@ if ((($_FILES["imgfile"]["type"] == "image/gif")
             //}
             //echo abc($output);
             //echo(strlen('Common wheat '));
-            echo $t;
+            //echo $t;
+            //$t=0;
+
+
+
             if($t==0)
             {
                 echo"We believe this plant belongs to Black-grass ";
                 //sleep(2);
-                echo ("<script>self.location=('/Black-grass');</script>");
+                setcookie('mycookie',$kl);
+                echo ("<script>self.location=('/Black-grass.php');</script>");
+
+
             }
             if($t==1)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Charlock ";
                 //sleep(2);
-                echo ("<script>self.location=('/Charlock');</script>");
+                echo ("<script>self.location=('/Charlock.php');</script>");
             }
             if($t==2)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Cleavers ";
                 //sleep(2);
-                echo ("<script>self.location=('/Cleavers');</script>");
+                echo ("<script>self.location=('/Cleavers.php');</script>");
             }
             if($t==3)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Common Chickweed";
                 //sleep(2);
-                echo ("<script>self.location=('/Common Chickweed');</script>");
+                echo ("<script>self.location=('/Common-Chickweed.php');</script>");
             }
             if($t==4)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Common wheat ";
                 //sleep(2);
-                echo ("<script>self.location=('/Common wheat');</script>");
+                echo ("<script>self.location=('/Common-wheat.php');</script>");
             }
             if($t==5)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Fat Hen ";
                 //sleep(2);
-                echo ("<script>self.location=('/Fat Hen');</script>");
+                echo ("<script>self.location=('/Fat-Hen.php');</script>");
             }
 
             if($t==6)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Loose Silky-bent ";
                 //sleep(2);
-                echo ("<script>self.location=('/Loose Silky-bent');</script>");
+                echo ("<script>self.location=('/Loose-Silky-bent.php');</script>");
             }
             if($t==7)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Maize ";
                 //sleep(2);
-                echo ("<script>self.location=('/Maize');</script>");
+                echo ("<script>self.location=('/Maize.php');</script>");
             }
             if($t==8)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Scentless Mayweed ";
                 //sleep(2);
-                echo ("<script>self.location=('/Scentless Mayweed');</script>");
+                echo ("<script>self.location=('/Scentless-Mayweed.php');</script>");
             }
             if($t==9)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Shepherds Purse ";
                 //sleep(2);
-                echo ("<script>self.location=('/Shepherds Purse');</script>");
+                echo ("<script>self.location=('/Shepherds-Purse.php');</script>");
             }
             if($t==10)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Small-flowered Cranesbill";
                 //sleep(2);
-                echo ("<script>self.location=('/Small-flowered Cranesbill');</script>");
+                echo ("<script>self.location=('/Small-flowered-Cranesbill.php');</script>");
             }
             if($t==11)
-            {
+            {setcookie('mycookie',$kl);
                 echo"We believe this plant belongs to Sugar beet";
                 //sleep(2);
-                echo ("<script>self.location=('/Sugar beet');</script>");
+                echo ("<script>self.location=('/Sugar-beet.php');</script>");
             }
 
-    }
+
 
 }
 }
